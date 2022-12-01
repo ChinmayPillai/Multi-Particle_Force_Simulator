b09417deb2c8690dd92451d527572a8ef407a16f
ChinmayPillai <113179689+ChinmayPillai@users.noreply.github.com>
2022-12-02 02:53:49 +0530

    Add files via upload

---
+++ TimePeriods.py
@@ -0,0 +1,50 @@
+import csv
+import numpy as np
+
+
+n = 9
+
+print("Time Period of Body:\n")
+
+for j in range(0,n,1):
+    file_name = 'Output\Evolution_' + str(j) +'.csv'
+    with open(file_name, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        start = False
+        switchTime = []
+        timeP = []
+        timePeriod = 0
+        prev = 0
+
+        for line in csv_reader:
+            
+            curr = np.sign(float(line[1]))
+
+            if(not start):
+                prev = curr
+                start = True
+            else:
+                if (curr != prev):
+                    switchTime.append(float(line[0]))
+                    prev = curr
+        
+        length = len(switchTime)
+
+        if(length <= 1):
+            print(f"{j} = Not enough Information\n")
+            #print("------------------------------------------------\n")
+            continue
+        elif(length == 2):
+            print("Aprroximation")
+            timePeriod = 2 * (switchTime[1]-switchTime[0]) / (60*60*24)
+        else:
+            for i in range(length-2):
+                timeP.append(switchTime[i+2]-switchTime[i])
+                #print((switchTime[i+2]-switchTime[i])/(60*60*24), end=", ")
+            timePeriod = sum(timeP)/len(timeP)
+            timePeriod = timePeriod / (60*60*24)
+
+        print(f"{j} = {timePeriod} Days\n")
+            #print("------------------------------------------------\n")    
+        
