d9f527fcfe2b3944d4b6f6cea55ed48abaf168fb
ChinmayPillai <113179689+ChinmayPillai@users.noreply.github.com>
2022-12-06 09:27:29 +0530

    Update Eccentricity.py

--- Eccentricity.py
+++ Eccentricity.py
@@ -1,55 +1,67 @@
+'''
+Find Eccentricity of Orbits of Particles from CSV File
+'''
+
 import csv
 import numpy as np
 
 
-n = 9
+def Eccentricity(n, filepath):
+    '''
+    Code to find the eccentricity of orbit of every particle
 
-print("Eccentricity of Orbit of Body:\n")
+    Args:
+        n (int): Number of Particles
+        filepath (string): Path of code location
+    '''
 
-for j in range(0,n,1):
-    file_name = 'Output\Evolution_' + str(j) +'.csv'
-    with open(file_name, 'r') as csv_file:
-        csv_reader = csv.reader(csv_file)
 
-        start = False
-        max = -1
-        min = 1e15
-        prev = 0
-        switch = 0
+    print("Eccentricity of Orbit of Body:\n")
 
-        for line in csv_reader:
-            
-            pt = np.array([float(line[1]), float(line[2]), float(line[3])])
-            curr = np.linalg.norm(pt)
-            sign = np.sign(float(line[1]))
+    for j in range(0,n,1):
+        file_name = filepath + 'Output\\Evolution_' + str(j) +'.csv'
+        with open(file_name, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
 
-            if(not start):
-                max = curr
-                min = curr
-                prev = sign
-                start = True
-                continue
-            else:
-                if (sign != prev):
-                    prev = sign
-                    switch += 1
+            start = False
+            max = -1
+            min = 1e15
+            prev = 0
+            switch = 0
 
-            if(switch == 1 or switch == 2):
-                if (max < curr):
+            for line in csv_reader:
+                
+                pt = np.array([float(line[1]), float(line[2]), float(line[3])])
+                curr = np.linalg.norm(pt)
+                sign = np.sign(float(line[1]))
+
+                if(not start):
                     max = curr
-                if (curr < min):
                     min = curr
-            elif(switch > 2):
-                break
+                    prev = sign
+                    start = True
+                    continue
+                else:
+                    if (sign != prev):
+                        prev = sign
+                        switch += 1
 
-    if(switch < 2):
-        print(f"{j}: Not Enough Information\n")
-    else:
-        if(switch == 2):
-            print("Approximation")
-        #print(f"{j}:\nMax: {max/1e10}, Min: {min/1e10}\n")
-        a = (max + min) / 2
-        b = np.sqrt(max * min)
-        e = np.sqrt(1 - ((b/a)**2))
-        print(f"{j} = {e}\n")
-        
+                if(switch == 1 or switch == 2):
+                    if (max < curr):
+                        max = curr
+                    if (curr < min):
+                        min = curr
+                elif(switch > 2):
+                    break
+
+        if(switch < 2):
+            print(f"{j}: Not Enough Information\n")
+        else:
+            if(switch == 2):
+                print("Approximation")
+            #print(f"{j}:\nMax: {max/1e10}, Min: {min/1e10}\n")
+            a = (max + min) / 2
+            b = np.sqrt(max * min)
+            e = np.sqrt(1 - ((b/a)**2))
+            print(f"{j} = {e}\n")
+
