6561888d080fb4c2619027b4a012c87084c1594d
ChinmayPillai <113179689+ChinmayPillai@users.noreply.github.com>
2022-12-06 09:28:39 +0530

    Update TimePeriods.py

--- TimePeriods.py
+++ TimePeriods.py
@@ -1,50 +1,62 @@
+'''
+Find Time Period of Orbits of Particles from CSV File
+'''
+
 import csv
 import numpy as np
 
 
-n = 9
+def TimePeriod(n,filepath):
+    '''
+    Code to find the time period of revolution of every particle
 
-print("Time Period of Revolution of Body:\n")
+    Args:
+        n (int): Number of Particles
+        filepath (string): Path of code location
+    '''
 
-for j in range(0,n,1):
-    file_name = 'Output\Evolution_' + str(j) +'.csv'
-    with open(file_name, 'r') as csv_file:
-        csv_reader = csv.reader(csv_file)
 
-        start = False
-        switchTime = []
-        timeP = []
-        timePeriod = 0
-        prev = 0
+    print("Time Period of Revolution of Body:\n")
 
-        for line in csv_reader:
-            
-            curr = np.sign(float(line[1]))
+    for j in range(0,n,1):
+        file_name = filepath + 'Output\\Evolution_' + str(j) +'.csv'
+        with open(file_name, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
 
-            if(not start):
-                prev = curr
-                start = True
-            else:
-                if (curr != prev):
-                    switchTime.append(float(line[0]))
+            start = False
+            switchTime = []
+            timeP = []
+            timePeriod = 0
+            prev = 0
+
+            for line in csv_reader:
+                
+                curr = np.sign(float(line[1]))
+
+                if(not start):
                     prev = curr
-        
-    length = len(switchTime)
+                    start = True
+                else:
+                    if (curr != prev):
+                        switchTime.append(float(line[0]))
+                        prev = curr
+            
+        length = len(switchTime)
 
-    if(length <= 1):
-        print(f"{j} = Not enough Information\n")
-        #print("------------------------------------------------\n")
-        continue
-    elif(length == 2):
-        print("Aprroximation")
-        timePeriod = 2 * (switchTime[1]-switchTime[0]) / (60*60*24)
-    else:
-        for i in range(length-2):
-            timeP.append(switchTime[i+2]-switchTime[i])
-            #print((switchTime[i+2]-switchTime[i])/(60*60*24), end=", ")
-        timePeriod = sum(timeP)/len(timeP)
-        timePeriod = timePeriod / (60*60*24)
+        if(length <= 1):
+            print(f"{j} = Not enough Information\n")
+            #print("------------------------------------------------\n")
+            continue
+        elif(length == 2):
+            print("Aprroximation")
+            timePeriod = 2 * (switchTime[1]-switchTime[0]) / (60*60*24)
+        else:
+            for i in range(length-2):
+                timeP.append(switchTime[i+2]-switchTime[i])
+                #print((switchTime[i+2]-switchTime[i])/(60*60*24), end=", ")
+            timePeriod = sum(timeP)/len(timeP)
+            timePeriod = timePeriod / (60*60*24)
 
-    print(f"{j} = {timePeriod} Days\n")
-    #print("------------------------------------------------\n")    
-        
+        print(f"{j} = {timePeriod} Days\n")
+        #print("------------------------------------------------\n")    
+ 
